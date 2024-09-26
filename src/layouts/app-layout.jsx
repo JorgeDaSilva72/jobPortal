e70507675f7e5307f8c3d037f85b05f346a14800
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-09-26 08:11:08 +0200

    Supabase and Clerk setup and connecting them.

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by AFRIQUE AVENIR
+        Réalisé avec 💗 par AFRIQUE AVENIR.
       </div>
     </div>
   );
