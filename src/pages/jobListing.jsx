5a383af2691ea6351d5cf134bbf2885f40a8b3e8
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-09-26 20:04:37 +0200

    Creating Job Listing Page .

--- src/pages/jobListing.jsx
+++ src/pages/jobListing.jsx
@@ -1,5 +1,78 @@
-const jobListing = () => {
-  return <div>jobListing</div>;
+import { useEffect, useState } from "react";
+
+import { getJobs } from "@/api/apiJobs";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
+import JobCard from "@/components/job-card";
+
+const JobListing = () => {
+  // avant le hook useFetch()
+  // const { session } = useSession();
+  // const fetchJobs = async () => {
+  //   const supabaseAccessToken = await session.getToken({
+  //     template: "supabase",
+  //   });
+  //   const data = await getJobs(supabaseAccessToken);
+  //   console.log(data);
+  // };
+
+  // useEffect(() => {
+  //   fetchJobs();
+  // }, []);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+  const { isLoaded } = useUser();
+
+  const {
+    loading: loadingJobs,
+    data: jobs,
+    fn: fnJobs,
+  } = useFetch(getJobs, {
+    location,
+    company_id,
+    searchQuery,
+  });
+
+  console.log(jobs);
+
+  useEffect(() => {
+    if (isLoaded) fnJobs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded, location, company_id, searchQuery]);
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Dernières annonces
+      </h1>
+
+      {loadingJobs && (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      )}
+
+      {loadingJobs === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {jobs?.length ? (
+            jobs.map((job) => {
+              return (
+                <JobCard
+                  key={job.id}
+                  job={job}
+                  savedInit={job?.saved?.length > 0}
+                />
+              );
+            })
+          ) : (
+            <div>No Jobs Found 😢</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
-export default jobListing;
+export default JobListing;
