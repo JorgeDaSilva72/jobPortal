71aa78596b2620f75fc92e7e26f55c567a53a75d
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2024-09-27 20:37:33 +0200

    Job page continued..

--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -14,6 +14,7 @@ import {
 
 import useFetch from "@/hooks/use-fetch";
 import { getSingleJob } from "@/api/apiJobs";
+import MDEditor from "@uiw/react-md-editor";
 
 const JobPage = () => {
   const { id } = useParams();
@@ -31,14 +32,19 @@ const JobPage = () => {
     if (isLoaded) fnJob();
   }, [isLoaded]);
 
+  const handleStatusChange = (value) => {
+    const isOpen = value === "open";
+    fnHiringStatus(isOpen).then(() => fnJob());
+  };
+
   if (!isLoaded || loadingJob) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
 
   return (
     <div className="flex flex-col gap-8 mt-5">
-      <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
-        <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl">
+      <div className="flex flex-col-reverse gap-6  justify-center items-center">
+        <h1 className="gradient-title font-extrabold pb-3 text-center md:text-left text-4xl sm:text-6xl">
           {job?.title}
         </h1>
         <img src={job?.company?.logo_url} className="h-12" alt={job?.title} />
@@ -80,6 +86,17 @@ const JobPage = () => {
           )}
         </div>
       </div>
+
+      <h2 className="text-2xl sm:text-3xl font-bold">À propos du poste</h2>
+      <p className="sm:text-lg">{job?.description}</p>
+
+      <h2 className="text-2xl sm:text-3xl font-bold">
+        Ce que nous recherchons :
+      </h2>
+      <MDEditor.Markdown
+        source={job?.requirements}
+        className="bg-transparent sm:text-lg" // add global ul styles - tutorial
+      />
     </div>
   );
 };
